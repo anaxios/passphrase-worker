41f43a7b0cc2eb2e6e0d9bac1066ee70c58150f8
anaxios <ilguappo@pm.me>
2024-08-06 07:13:18 -0700

    add routing for random with token

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import { Hono } from 'hono'
+import { Passphrase, RandomLocal, RandomAPI } from './random-words.ts'
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
+
+function numberSanitize(number) {
+  return Math.min(Math.floor(Math.abs(number)), 1000) || 6;
+}
+
+app.get('/api/random/:count?', async (c) => {
+  const { count } = c.req.param();
+  const passphrase = new Passphrase(new RandomLocal(numberSanitize(count)));
+  return c.text(await passphrase.roll());
+});
+
+app.get('/api/random/:count?/token/:token?', async (c) => {
+  const { count, token } = c.req.param();
+  if (token) {
+    const passphrase = new Passphrase(new RandomAPI(numberSanitize(count), token));
+    return c.text(await passphrase.roll());
+  }
+  const passphrase = new Passphrase(new RandomLocal(numberSanitize(count)));
+  return c.text(await passphrase.roll());
+});
 
 export default app
